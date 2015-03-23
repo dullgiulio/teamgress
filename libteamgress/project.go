d0dc30edf9ad8b8ae50087fed41e6385fa9fc1b2
Giulio Iotti <dullgiulio@gmail.com>
2015-03-23 08:08:54 +0000

    Add midding file for project type

---
+++ libteamgress/project.go
@@ -0,0 +1,25 @@
+package libteamgress
+
+import (
+	"fmt"
+)
+
+type Project struct {
+	Name    string `json:"name"`
+	Service string `json:"service,omitempty"`
+	Stage   string `json:"stage,omitempty"`
+	Server  string `json:"server,omitempty"`
+	Avatar  string `json:"stage,omitempty"`
+}
+
+func MakeProject(name, service, stage string) Project {
+	return Project{
+		Name:    name,
+		Service: service,
+		Stage:   stage,
+	}
+}
+
+func (p *Project) String() string {
+	return fmt.Sprintf("%s/%s [%s]", p.Name, p.Service, p.Stage)
+}
